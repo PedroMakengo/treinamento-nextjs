0843325bde14f6fffe86703890a8359d6506bce6
PedroMakengo <pedromakengomiguel@gmail.com>
2023-02-01 20:18:54 +0100

    Aprendendo a usar o loading

--- 02-projeto/repos/src/Pages/Main/index.tsx
+++ 02-projeto/repos/src/Pages/Main/index.tsx
@@ -1,14 +1,68 @@
-import { FaGithub } from "react-icons/fa";
+import React, { useState, useCallback } from "react";
 
-import { Container } from "./style";
+import { FaGithub, FaPlus, FaSpinner } from "react-icons/fa";
+
+import { Container, Form, SubmitButton } from "./style";
+
+import api from "../../services/api";
+
+interface Repository {
+  name: string;
+}
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositorios, setRepositorios] = useState<Repository[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = useCallback(
+    (event: React.FormEvent) => {
+      event.preventDefault();
+      async function submit() {
+        setLoading(true);
+        try {
+          const response = await api.get(`repos/${newRepo}`);
+
+          const data = {
+            name: response.data.full_name,
+          };
+
+          setRepositorios([...repositorios, data]);
+          setNewRepo("");
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+      submit();
+    },
+    [newRepo, repositorios]
+  );
+
   return (
     <Container>
       <h1>
         <FaGithub size="25" />
         Meus repositórios
       </h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Adicionar repositórios"
+        />
+
+        <SubmitButton>
+          {loading ? (
+            <FaSpinner color="#fff" size="14" />
+          ) : (
+            <FaPlus color="#fff" size="14" />
+          )}
+        </SubmitButton>
+      </Form>
     </Container>
   );
 }
--- 02-projeto/repos/src/Pages/Main/style.ts
+++ 02-projeto/repos/src/Pages/Main/style.ts
@@ -1,5 +1,56 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  color: red;
+  max-width: 700px;
+  background-color: #fff;
+  border-radius: 4px;
+
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  padding: 30px;
+  margin: 80px auto;
+
+  h1 {
+    display: flex;
+    font-size: 20px;
+    flex-direction: row;
+    align-items: center;
+
+    gap: 1rem;
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+
+  display: flex;
+  flex-direction: row;
+  gap: 0.5rem;
+
+  input {
+    flex: 1;
+    border: 1px solid #ddd;
+    padding: 10px 15px;
+    border-radius: 4px;
+
+    font-size: 17px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: "submit",
+})`
+  background: #0d2636;
+  border: 0;
+  border-radius: 4px;
+
+  padding: 0 15px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
 `;
